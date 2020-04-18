58d85d5edd339ba29a8f5dc904ab39f7c25e6c12
Hongbin.Yuan <yuanhongbin9090@gmail.com>
2020-04-18 23:43:53 +0800

    delete fb and amazon

--- main/main.go
+++ main/main.go
@@ -1,15 +1,6 @@
 package main
 
-import (
-	"fmt"
-	"github.com/yuanmomo/go-cidrman/list"
-)
-
 func main()  {
-	all := list.GetAll();
-	for _,cidr := range all {
-		fmt.Printf("%s\n",cidr)
-	}
 
 }
 
